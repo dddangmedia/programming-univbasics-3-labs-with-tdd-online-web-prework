d4df1a021dd0f575bca20ea8722ac11f109e795b
Duong Dang <dddangmedia@gmail.com>
2019-09-03 22:19:04 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,2 +1,14 @@
 first_number = 2 
 second_number = 1
+
+def sum(first_number, second_number)
+  sum = first_number + second_number
+  puts sum  
+end
+
+def difference(first_number, second_number)
+  difference = first_number + second_number
+  puts difference
+end
+
+def 
